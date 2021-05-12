b1cc7efe4f5c9346e5726d241beb798bae339b77
gitbash <kolhenagesh2017@gmail.com>
2021-05-13 00:51:37 +0530

    Added: Comparison of lines

--- LineComp.java
+++ LineComp.java
@@ -1,14 +1,14 @@
 import static java.lang.Math.sqrt;
 public class LineComp {
 
-public static final int x1=3;
-public static final int x2=5;
-public static final int y1=3;
-public static final int y2=5;
-public static final int x3=3;
-public static final int x4=5;
-public static final int y3=3;
-public static final int y4=5;
+public static final int x1=10;
+public static final int x2=20;
+public static final int y1=5;
+public static final int y2=15;
+public static final int x3=17;
+public static final int x4=20;
+public static final int y3=25;
+public static final int y4=30;
 
 public static void main(String[] args) {
 
@@ -21,8 +21,11 @@ double l2 =Math.round( sqrt( ((x4-x3) * (x4-x3)) + ((y4-y3) * (y4-y3)) ));
   if ( l1 == l2) {
      System.out.println("l1 is equal to l2");
   }
+  else if( l1 > l2) {
+     System.out.println("l1 is greater to l2");
+  }
   else{
-     System.out.println("l1 is not equal to l2");
+     System.out.println("l1 is less than l2");
   }
  }
 }
