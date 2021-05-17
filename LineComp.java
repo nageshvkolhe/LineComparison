0983d8c4942fdb284335fb6f541ed90c1928ee80
gitbash <kolhenagesh2017@gmail.com>
2021-05-17 23:21:02 +0530

    Added : Java Oriented programming concepts

--- LineComp.java
+++ LineComp.java
@@ -1,33 +1,45 @@
 import static java.lang.Math.sqrt;
-public class LineComp {
+  public class LineComp {
 
-   public static final int x1 = 10;
-   public static final int x2 = 20;
-   public static final int y1 = 5;
-   public static final int y2 = 15;
-   public static final int x3 = 17;
-   public static final int x4 = 20;
-   public static final int y3 = 25;
-   public static final int y4 = 30;
+  private final int x1;
+  private final int x2;
+  private final int y1;
+  private final int y2;
+  private final int x3;
+  private final int x4;
+  private final int y3;
+  private final int y4;
 
+  public LineComp(int x1, int x2, int y1, int y2, int x3, int y3, int x4, int y4) {
+  this.x1 = x1;
+  this.x2 = x2;
+  this.y1 = y1;
+  this.y2 = y2;
+  this.x3 = x3;
+  this.x4 = x4;
+  this.y3 = y3;
+  this.y4 = y4;
+ }
+ public void comparision(){
+ double l1 =Math.round( sqrt( ((x2-x1) * (x2-x1)) + ((y2-y1) * (y2-y1)) ));
+  System.out.println("l1:" +l1);
 
-   private static void line() {
+ double l2 =Math.round( sqrt( ((x4-x3) * (x4-x3)) + ((y4-y3) * (y4-y3)) ));
+  System.out.println("l2:" +l2);
 
-     double l1 =Math.round( sqrt( ((x2-x1) * (x2-x1)) + ((y2-y1) * (y2-y1)) ));
-     System.out.println("l1:" +l1);
+   if ( l1 == l2) {
+      System.out.println("l1 is equal to l2");
+   }
+   else if( l1 > l2) {      System.out.println("l1 is greater to l2");
+   }
+   else{
+      System.out.println("l1 is less than l2");
+   }
+ }
 
-     double l2 =Math.round( sqrt( ((x4-x3) * (x4-x3)) + ((y4-y3) * (y4-y3)) ));
-     System.out.println("l2:" +l2);
-
-        if ( l1 == l2)
-            System.out.println("l1 is equal to l2");
-        else if( l1 > l2)
-            System.out.println("l1 is greater to l2");
-        else
-            System.out.println("l1 is less than l2");
-     }
-
-        public static void main(String[] args) {
-        line();
-        }
-}
+ public static void main (String[] args){
+   LineComp firstvalue = new LineComp(3,4,5,6,7,8,9,10);
+   firstvalue.comparision();
+   System.out.println(firstvalue);
+ }
+ }
